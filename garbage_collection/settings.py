980491abe4be59865e274f037c24d7aa843fda05
Hassanyange <hassanyange@gmail.com>
2024-03-27 07:14:24 +0300

    chaneges on staticfiles

--- garbage_collection/settings.py
+++ garbage_collection/settings.py
@@ -118,15 +118,17 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'  # Add a leading slash
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
