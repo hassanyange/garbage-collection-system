82dcda91e6b139e587e3503b48eb70564cb3da04
benny-png <mazikuben2@gmail.com>
2024-07-03 20:01:12 +0300

    some changes

--- garbage_collection/urls.py
+++ garbage_collection/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('collection_system.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 
